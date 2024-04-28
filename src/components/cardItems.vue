0b3fb398a469f1003e88f4df5293cbecad6b3af5
giahuy215 <giahuy4842@gmail.com>
2024-04-28 13:05:43 +0700

    feature: add modal when hover cardItems

--- src/components/cardItems.vue
+++ src/components/cardItems.vue
@@ -1,12 +1,26 @@
 <template>
-	<div class="flex flex-col w-full max-w-[400px] h-[585px]">
-		<div class="relative w-full">
+	<div class="font-default flex flex-col w-full max-w-[400px] h-[585px]">
+		<div class="items relative w-full">
 			<img
 				class="w-full max-h-[500px] object-cover object-center rounded"
 				:src="listItems.image"
 			/>
 			<div class="absolute top-3 right-3">
 				<Badges :listBadges="listItems.badges" />
+			</div>
+			<div class="items-modal w-full h-10 bottom-3">
+				<div
+					class="flex flex-row mx-3 px-3 py-2 bg-white justify-between rounded"
+				>
+					<p class="text-sm font-medium leading-6 uppercase">
+						<slot name="modal-content">View Recipe</slot>
+					</p>
+					<img
+						class="w-6 h-6"
+						src="../assets/icons/arrow-up-right-icon.svg"
+						loading="lazy"
+					/>
+				</div>
 			</div>
 		</div>
 		<div class="flex flex-col w-full h-1/3">
@@ -21,14 +35,12 @@
 								:src="detail.icons"
 							/>
 						</template>
-						<p class="font-default text-sm">{{ detail.content }}</p>
+						<p class="text-sm">{{ detail.content }}</p>
 					</TagDetailPrep>
 				</template>
 			</div>
 			<div>
-				<span class="font-default text-2xl font-bold">{{
-					listItems.name
-				}}</span>
+				<span class="text-2xl font-bold">{{ listItems.name }}</span>
 			</div>
 		</div>
 	</div>
@@ -46,5 +58,18 @@
 <style scoped>
 	.font-default {
 		font-family: "Syne", sans-serif;
+	}
+
+	.items-modal {
+		position: absolute;
+		opacity: 0;
+		transform: translateY(10px);
+	}
+
+	.items:hover .items-modal {
+		display: block;
+		transition: all 0.5s ease;
+		opacity: 1;
+		transform: translateY(0);
 	}
 </style>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -124,14 +124,12 @@
 
 	.comp {
 		opacity: 0;
-		/* padding-top: 50px; */
 		transform: translateY(50px);
 	}
 
 	.comp-animation {
 		transition: all 1s ease-in-out;
 		opacity: 1;
-		/* padding-top: 0; */
 		transform: translateY(0);
 	}
 </style>
